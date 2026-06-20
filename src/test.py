06411cdabefe95f8729688bffd545d9ddf857203
eugene :) <willms.julius@gmx.de>
2026-06-20 13:33:11 +0200

    feat: est. print time + print starting mag

--- src/test.py
+++ src/test.py
@@ -25,5 +25,14 @@ if __name__ == "__main__":
     print(f"{len(schematic.blocklist)} blocks to print")
     print(f"{len(instructions) - len(schematic.blocklist)} mag changes")
     schematic.create_schematic_for_block_assigment(block_stream_manager.block_palette)
+    print(
+        f"Print is starting at mag index (0-based): {block_stream_manager.block_stream[0].mag_index}"
+    )
+    block_count = len(schematic.blocklist)
+    mag_changes = len(instructions) - len(schematic.blocklist)
+    est_gt = (
+        (block_count * 10) + (block_count * 2) + (mag_changes * 4) + (mag_changes * 20)
+    )
+    print(f"Est. print time: {est_gt}gt's ({est_gt / 20 / 60}min)")
     sleep(3)
     key_manager.type_intructions(schematic.name, 0, "10,10", "2", instructions)
